72fc8967620f0968c7402c89885b4ff8de602b50
Dan Cox <dan.cox@videlais.com>
2020-09-15 05:27:09 -0400

    Week 4 Assignment

---
+++ week4/index.js
@@ -0,0 +1,20 @@
+import Sidebar from './components/Sidebar.js';
+
+const s = new Sidebar({
+  menu: [
+    'Albert Eggstein',
+    'Attila the Hen',
+    'Attila the Hen',
+    'Dixie Chick',
+    'Gregory Peck',
+    'Mary Poopins'
+  ]
+});
+
+/* 
+  TODO: Use the render() method of Sidebar to show HTML response on console. Results should be wrapped in <ul></ul>
+  
+  The final result should look like the following:
+  <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
+*/
+console.log();
