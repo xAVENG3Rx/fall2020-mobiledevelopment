9c3b6f12ab405b19859bd497a42fb47afdc9a549
Jonathan J Montalvo <36077725+xAVENG3Rx@users.noreply.github.com>
2020-10-10 17:10:06 -0400

    Update App.js

--- week7/src/App.js
+++ week7/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+  
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Count: {this.state.counter}</p>
+          <button  onClick={() => {this.setState({counter: this.state.counter +1})}}>Increase!</button>
+        </header>
+      </div>
+    );
+  }
+}
 export default App;
