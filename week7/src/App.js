7edbe96242b9d3969d52c998eb7854b60e1fe798
Jonathan J Montalvo <36077725+xAVENG3Rx@users.noreply.github.com>
2020-10-24 20:48:42 -0400

    Portfolio 1

--- week7/src/App.js
+++ week7/src/App.js
@@ -16,10 +16,13 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>Count: {this.state.counter}</p>
-          <button onClick={() => {this.setState({counter: this.state.counter +1})}}>Increase!</button>
-          <button onClick={() => {this.setState({counter: this.state.counter *0})}}>Reset</button>
+          <p>Counter: {this.state.counter}</p>
+          <button className="button1" onClick={() => {this.setState({counter: this.state.counter +1})}}>+1</button>
+          <button className="button2" onClick={() => {this.setState({counter: this.state.counter -1})}}>-1</button>
+          <br></br>
+          <button className="button3" onClick={() => {this.setState({counter: this.state.counter *0})}}>Reset</button>
         </header>
+        <p>Created by Jonathan Montalvo Colon</p>
       </div>
     );
   }
